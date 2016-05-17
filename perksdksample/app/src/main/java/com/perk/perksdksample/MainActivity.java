8712b2f5ddd6aee12f5defff9f96f05f41eb9a01
lakshmana <lakshmana@perk.com>
2016-05-17 13:30:39 +0530

    bumped up version for release

--- perksdksample/app/src/main/java/com/perk/perksdksample/MainActivity.java
+++ perksdksample/app/src/main/java/com/perk/perksdksample/MainActivity.java
@@ -273,7 +273,7 @@ public class MainActivity extends Activity implements PerkAppInterface {
             public void run() {
                 logScrollView.fullScroll(View.FOCUS_DOWN);
             }
-        }, 300);
+        }, 200);
     }
 
 
